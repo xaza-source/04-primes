be1a0cafff50a76722111de143874259813546e2
xaza-source <sofiane.selmet@edu.esiee.fr>
2025-11-13 09:38:07 +0100

    Implement initial structure for syracuse function
    Define the syracuse function and main execution flow.

---
+++ python-04-fonctions-suites-syracuse.py
@@ -0,0 +1,23 @@
+def syracuse(n):
+
+    pass
+
+    # initialisation des variables
+
+    # tant que la suite n'est pas terminée :
+    #   - calcul du n suivant
+    #   - mise à jour du temps de vol (tv)
+    #   - mise à jour du temps de vol en altitude (tva) si nécessaire
+    #   - mise à jour de l'altitude maximale (am)
+
+    # retour de tv, tva, am
+
+
+def main():
+    # exemple d'exécution
+    n = 15
+    tv, tva, am = syracuse(n)
+    print(n, tv, tva, am)
+
+if __name__ == "__main__":
+    main()
